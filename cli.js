7bcd36eceb7519fc5b45f20cf9d18cd8c9758849
Temporary <temporary@Temporarys-MacBook-Air.local>
2020-01-30 00:51:37 -0800

    merge future brach

--- cli.js
+++ cli.js
@@ -2,19 +2,20 @@
 var sourceMap = require('source-map');
 var fs = require('fs');
 var child = require('child_process');
-var chalk = require('chalk');
-var path = require('path')
+const chalk = require('chalk');
+const path = require('path')
 
-const basePath = process.env.PWD;
-const sourceMapPath = basePath + '/source-maps/';
+let basePath = process.env.PWD;
+let sourceMapPath = basePath + '/source-maps/';
 
-const command = process.argv[2];
+let command = process.argv[2];
 
 const y = x => chalk.yellow(x);
 const g = x => chalk.green(x);
 const b = x => chalk.blue(x);
 const r = x => chalk.red(x);
 const m = x => chalk.magenta(x);
+const bld = x => chalk.bold(x)
 
 var logs = [];
 var data;
@@ -23,7 +24,7 @@ var data;
     if(command == "with"){
         let customPathArg = process.argv[3];
         let customPath = path.resolve(basePath, customPathArg)
-        let f = require(customPath)
+        var f = require(customPath)
         rawData = await f();
         data = Object.values(rawData)
     } else {
@@ -34,29 +35,28 @@ var data;
     await Promise.all(data.map(async e=>{
         let output = '\n';
         try{
-            output += "timestamp : " + y( e.timestamp ) + '\n';
-            output += "date/time : " + y( new Date(e.timestamp).toUTCString() ) + '\n';
+            output += bld("timestamp:\n  ") + y( e.timestamp ) + '\n';
+            output += y( '  ' + new Date(e.timestamp).toUTCString() ) + '\n';
             if(e.message){
-                output += "error message : " + m(e.message) + '\n';
+                output += bld("error message:\n  ") + m(e.message) + '\n';
             } ;
             let userData = Object.keys(e).filter(k=>!['stack','message','timestamp','mapId'].includes(k));
             if(userData){
-                output += y('user data: {') + '\n';
+                output += bld('user data: {') + '\n';
             };
-            userData.forEach(k=> output += '  ' + k + ': ' + e[k] + '\n');
+            userData.forEach(k=> output += '  ' + g(k) + ': ' + b(e[k]) + '\n');
             output += '}\n';
-            // will catch here if cant find source map
             let map = fs.readFileSync(sourceMapPath + e.mapId +'.map', 'utf8');
-            output += 'stack trace:\n';
+            output += bld('stack trace:\n');
             let traces = e.stack.split('\n');
             let traceObjects = traces.map(t=>({
-                column : t.split(':')[t.split(':').length-1],
-                row : t.split(':')[t.split(':').length-2],
-                name : t.split('@')[0]
+                    column : t.split(':')[t.split(':').length-1],
+                    row : t.split(':')[t.split(':').length-2],
+                    name : t.split('@')[0]
                 }))
                 .filter(t=>t.column && t.row && t.name)
                 .map(t=>t.name.startsWith('http')?({row:t.row,column:t.column}):t);
-            let smc = await new sourceMap.SourceMapConsumer(map);
+            var smc = await new sourceMap.SourceMapConsumer(map);
             traceObjects.forEach(trace=>{
                 let line = parseInt(trace.row);
                 let column = parseInt(trace.column);
@@ -64,15 +64,16 @@ var data;
                     line: line,
                     column: column
                 });
-                output += b( error.name + ": " ) + error.source ;
-                output += y(' {line ' + error.line + ' : ' + error.column + '}') + '\n';
+                output += b( ' ' + error.name + ": " ) + error.source.replace(basePath,'.') ;
+                output += y(' {line ' + error.line + ':' + error.column + '}') + '\n';
             });
             output += '\n---------------------------------------------------------------------\n';
             logs.push({output,timestamp:e.timestamp});
         }catch(err){
-            output += r( 'LOGGER_ERROR: could not find source map at: ' + '\n' );
-            output += r( sourceMapPath + e.mapId + '.map'  + '\n' );
-            output += '\n-------------------------------------------------------------------';
+            output += r( 'ERROR: failed for source map at:\n' );
+            output += r( sourceMapPath + e.mapId + '.map\n' );
+            output += r( err )
+            output += '\n---------------------------------------------------------------------\n';
             logs.push({output,timestamp:e.timestamp});
         }
     }))
